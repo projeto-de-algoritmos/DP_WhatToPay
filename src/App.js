589d3853b41a3c84f09841d947070f9e2f28b70c
João Pedro <jps12carvalho@gmail.com>
2021-05-03 00:07:27 -0300

    Adicionando botao para colocar na lista

--- src/App.js
+++ src/App.js
@@ -14,17 +14,33 @@ class App extends Component {
       task_value: "",
       task_cost: "",
     }
+
+    this.addTaskList = this.addTaskList.bind(this)
   }
 
+  addTaskList(){
+    var task = {
+      name: this.state.task_name,
+      value: this.state.task_value,
+      cost: this.state.task_cost
+    }
+
+    this.state.task_list.push(task)
+
+    console.log(this.state.task_list)
+  }
 
   render(){
     return (
       <div className="App">
         <div className="Form">
-          <input type="text"></input>
-          <input type="text"></input>
-          <input type="text"></input>
-          <input type="button" value="add"></input>
+          <input type="text" onChange={(event) => {this.setState({task_name: event.target.value})}}></input>
+          <input type="text" onChange={(event) => {this.setState({task_value: event.target.value})}}></input>
+          <input type="text" onChange={(event) => {this.setState({task_cost: event.target.value})}}></input>
+          <input type="button" value="add" onClick={() => this.addTaskList()}></input>
+          <h4>{this.state.task_name}</h4>
+          <h4>{this.state.task_value}</h4>
+          <h4>{this.state.task_cost}</h4>
         </div>
         <div className="List">
         </div>
