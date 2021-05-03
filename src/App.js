24d8d8932ef6a827bd7ba3a143e6cc665dba8c6a
João Pedro <jps12carvalho@gmail.com>
2021-05-03 12:24:07 -0300

    Adicionando knapsack

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 import ListItem from './components/ListItem.js'
 import React, {Component} from 'react';
 
+function max(a, b){
+  return (a > b? a : b);
+}
+
 class App extends Component {
 
   constructor(props){
@@ -15,11 +19,14 @@ class App extends Component {
         name: "",
         value: 0,
         cost: 0,
-      }
+      },
+      message: "",
+      weight: 0,
     }
 
     this.addNewTasks = this.addNewTasks.bind(this)
     this.deleteTask = this.deleteTask.bind(this)
+    this.knapsack = this.knapsack.bind(this)
   }
 
   addNewTasks(task){
@@ -34,14 +41,66 @@ class App extends Component {
     this.setState({task_list})
   }
 
+  knapsack(){
+    const tasks = Array.from(this.state.task_list)
+    const n = tasks.length, W = this.state.weight
+
+    let K = new Array(n+1);
+    let i, w = Number();
+
+    for (i = 0; i < K.length; i++) {
+      K[i] = new Array(W+1)
+      for (w = 0; w < W; w++){
+        K[i][w] = -1;
+      }
+    }
+
+    for (i = 0; i <= n; i++) {
+      for (w = 0; w <= W; w++){
+        if (i === 0 || w === 0)
+          K[i][w] = 0
+        else if (tasks[i-1].cost <= w) 
+          K[i][w] = max(K[i-1][w - tasks[i-1].cost] + tasks[i-1].value, K[i-1][w]);
+        else
+          K[i][w] = K[i-1][w];
+      }
+    }
+
+    let best;
+    let res = new Number(K[n][W]);
+    res = K[n][W]
+    best = []
+    w = W
+
+    for (i = n; res > 0 && i > 0; i--){
+      console.log(typeof res, typeof K[i-1][w])
+      if (res === K[i-1][w])
+        continue
+      else {
+        best.push(tasks[i-1])
+        res -= tasks[i-1].value
+        w -= tasks[i-1].cost
+      }
+    }
+
+    const task_list = Array.from(best)
+    const message = "Knapsack feito"
+    this.setState({task_list})
+    this.setState({message})
+  }
+
   render(){
     return (
       <div className="App">
+        <div>
+          <input type="text" onChange={(event) => this.setState({weight: event.target.value})}></input>
+        </div>
         <div className="Form">
-          <input type="text" onChange={(event) => {this.setState({task: {...this.state.task, name: event.target.value}})}}></input>
-          <input type="text" onChange={(event) => {this.setState({task: {...this.state.task, value: event.target.value}})}}></input>
-          <input type="text" onChange={(event) => {this.setState({task: {...this.state.task, cost: event.target.value}})}}></input>
+          <input type="text" onChange={(event) => {this.setState({task: {...this.state.task, name: event.target.value}})}}></input> {/* Inserir o nome da conta*/}
+          <input type="text" onChange={(event) => {this.setState({task: {...this.state.task, value: event.target.value}})}}></input> {/*Inserir valor de benefício*/}
+          <input type="text" onChange={(event) => {this.setState({task: {...this.state.task, cost: event.target.value}})}}></input> {/*Inserir custo da conta*/}
           <input type="button" value="add" onClick={() => this.addNewTasks(this.state.task)}></input>
+        </div>
           {this.state.task_list.map(({id, name}, index) => (
             <ListItem
               key={id}
@@ -49,9 +108,8 @@ class App extends Component {
               onDelete={() => this.deleteTask(index)}
             />
           ))}
-        </div>
-        <div className="List">
-        </div>
+          <input type="button" onClick={() => this.knapsack()} value="Knapsack it"></input>
+          <h4>{this.state.message}</h4>
       </div>
     );
   }
